3a09eac799d0a73395122dc724421374a23856ab
A-Marcelli <marcelli.1901726@studenti.uniroma1.it>
2024-02-16 17:54:45 +0100

    aggiunti gli interrupt necessari alla uart

--- AVR/main_avr.c
+++ AVR/main_avr.c
@@ -8,11 +8,31 @@
 #include "./avr_common/my_uart.h"
 #include "./avr_common/my_adc.h"
 
-
+//interrupt
 ISR(ADC_vect)
 {
     //Interrupt che si attiva a conversione completata
 }
+
+ISR(USART0_RX_vect)
+{
+    //Interrupt che si attiva quando è stato ricevuto un byte
+}
+
+ISR(USART0_UDRE_vect)
+{
+    //Interrupt che si attiva quando il dato precendente è stato trasmesso, UDR0 vuoto
+}
+
+ISR(USART0_TX_vect)
+{
+    //Interrupt che si attiva quando è finita la trasmissione
+}
+
+
+//global variables
+volatile uint8_t adc_number;
+volatile uint8_t frequency;
 
 
 
@@ -23,5 +43,7 @@ int main(void){
     UART_init();  //inizializzazione uart
     adc_init();   //inizializzazione adc
     sei();        //interrupt abilitati
+
+
 
 }
